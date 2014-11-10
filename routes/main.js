eef6aa4aa3ab3c020c18117f4b1de0db8b72fa76
viehot <piri.viejo@gmail.com>
2014-11-10 14:07:29 -0300

    Edit user

--- routes/main.js
+++ routes/main.js
@@ -32,3 +32,32 @@ app.get('/p/delete/:id', function(req, res){
         }    
     });
 });
+
+app.get('/p/edit/:id', function(req, res){
+    Persons.findOne({ _id: req.params.id }, function(err, doc){
+        if(!err){
+            res.render('edit', { title: 'Edit', person: doc});
+        } else {
+            res.end(err);    
+        }    
+    });
+});
+
+app.post('/p/edit/:id', function(req, res){
+    console.log(req.body);
+     Persons.findOne({ _id: req.params.id }, function(err, doc){
+         if(!err){
+            doc.name = req.body.name; 
+            doc.age = req.body.age;
+            doc.save(function(err, doc){
+                if(!err){
+                    res.redirect('/list');
+                } else {
+                    res.end(err);    
+                }    
+            }); 
+        } else {
+            res.end(err);    
+        }    
+    });
+});
