b6715f65309d549c33f7d3b5d6acc17e51c2d9a4
viehot <piri.viejo@gmail.com>
2014-11-25 14:18:01 -0300

    Security method GET/POST

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,21 @@ var passport = module.parent.exports.passport;
 var Persons = require('../models/persons.js');
 var Admins = require('../models/admin.js');
 
+var adminAuth = function(req, res, next){
+    //authorize role
+    if(typeof req.user != "undefined"){
+        next();
+    }else{
+        //Not authorized redirect
+        res.redirect('/');
+    }
+}
+
+app.use(function(req, res, next) {
+    res.locals.user = req.user;
+    next();
+});
+
 app.get('/login', function(req, res){
     res.render('login', { title: 'Login'});
 });
@@ -12,19 +27,19 @@ app.post('/login', passport.authenticate('AdminLogin',
       failureRedirect: '/login',
       failureFlash: true }));
 
-app.get('/list', function(req, res){
+app.get('/list', adminAuth, function(req, res){
     var msg = req.flash('message');
     Persons.find({}, function(err, docs){
         res.render('list', { title: 'List', persons: docs, flashmsg: msg});
     });
 });
 
-app.get('/p/new', function(req, res){
+app.get('/p/new', adminAuth, function(req, res){
    req.flash('message', 'You visited /new');
    res.render('new', { title: 'New'});
 });
 
-app.post('/p/new', function(req, res){
+app.post('/p/new', adminAuth, function(req, res){
     console.log(req.body);
     var p = new Persons({ name: req.body.name, age: req.body.age });
     p.save(function(err, doc){
@@ -36,7 +51,7 @@ app.post('/p/new', function(req, res){
     });
  });
 
-app.get('/p/delete/:id', function(req, res){
+app.get('/p/delete/:id', adminAuth, function(req, res){
     Persons.remove({ _id: req.params.id }, function(err, doc){
         if(!err){
             res.redirect('/list');
@@ -46,7 +61,7 @@ app.get('/p/delete/:id', function(req, res){
     });
 });
 
-app.get('/p/edit/:id', function(req, res){
+app.get('/p/edit/:id', adminAuth, function(req, res){
     Persons.findOne({ _id: req.params.id }, function(err, doc){
         if(!err){
             res.render('edit', { title: 'Edit', person: doc});
@@ -56,7 +71,7 @@ app.get('/p/edit/:id', function(req, res){
     });
 });
 
-app.post('/p/edit/:id', function(req, res){
+app.post('/p/edit/:id', adminAuth, function(req, res){
     console.log(req.body);
      Persons.findOne({ _id: req.params.id }, function(err, doc){
          if(!err){
